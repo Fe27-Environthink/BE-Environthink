1705024867727d74f9db54d1dd9d7f86be749477
Shou9.123.Myko <mikofernando2003@gmail.com>
2023-09-02 20:46:25 +0700

    heheh

--- config/database.js
+++ config/database.js
@@ -5,9 +5,18 @@ const { Sequelize } = require("sequelize");
 //   dialect: "mysql",
 // });
 
-const db = new Sequelize("sql12630004", "sql12630004", "Q3bCn417uP", {
-  host: "sql12.freemysqlhosting.net",
-  dialect: "mysql",
-});
+// const db = new Sequelize("sql12630004", "sql12630004", "Q3bCn417uP", {
+//   host: "sql12.freemysqlhosting.net",
+//   dialect: "mysql",
+// });
+const db = new Sequelize(
+  "mikofirn_fahmi_test",
+  "mikofirn_fahmi_ajeh",
+  "fahmi.123.123",
+  {
+    host: "testfahmi.mikofirnando.my.id",
+    dialect: "mysql",
+  }
+);
 
 module.exports = db;
